8cfb5e4c712fb9502d168854aa182517cb6e3a36
Sumukh Jadhav <48212565+SumukhJadhav@users.noreply.github.com>
2020-12-24 16:14:06 +0530

    Update time.py

--- time.py
+++ time.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+import datetime
+dt_started = datetime.datetime.now()
+import time
+# do some stuff
+print(dt_started)
+time.sleep(5)
 
-now = datetime.now()
-
-current_time = now.strftime("%a %b %d %I:%M %p")
-
-#time.strftime("%a %b %d %I:%M:%S %p %Y")
-
-print("Current Time =", current_time)
+dt_ended = datetime.datetime.now()
+print((dt_ended - dt_started).total_seconds())
